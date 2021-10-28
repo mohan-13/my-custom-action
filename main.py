dd448125f3d5f6af9a673a17577ec70113320820
MOHANKUMAR T <mohan13081999@gmail.com>
2021-10-28 17:29:00 +0530

    fix

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ import os
 with open(os.environ.get("GITHUB_EVENT_PATH"), "r") as read_file:
     data = json.load(read_file)
     print(data)
-    requests.post(os.environ.get("INPUT_DISCORD-WEBHOOK-URL"),data={"content":data.issue.html_url})
+    requests.post(os.environ.get("INPUT_DISCORD-WEBHOOK-URL"),data={"content":data['issue']['html_url']})
